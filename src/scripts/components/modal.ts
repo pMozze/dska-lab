1c1ab5b61ce16dcfa29fc89b3673474b216d5c95
Maxim Tolkachev <maks.tolkachev.99@gmail.com>
2025-02-18 16:14:54 +0700

    kall

--- src/scripts/components/modal.ts
+++ src/scripts/components/modal.ts
@@ -1,10 +1,18 @@
 const modal = document.getElementById('modal') as HTMLElement;
+const successModal = document.getElementById('success-modal') as HTMLElement;
 const body = document.body as HTMLElement;
 
 function openModal() {
   modal.style.display = 'flex';
   modal.classList.remove('hide');
   modal.classList.add('show');
+  body.style.overflow = 'hidden';
+}
+
+function openSuccessModal() {
+  successModal.style.display = 'flex';
+  successModal.classList.remove('hide');
+  successModal.classList.add('show');
   body.style.overflow = 'hidden';
 }
 
@@ -23,6 +31,21 @@ function closeModal() {
   );
 }
 
+function closeSuccessModal() {
+  successModal.classList.remove('show');
+  successModal.classList.add('hide');
+
+  successModal.addEventListener(
+    'animationend',
+    () => {
+      successModal.classList.remove('hide');
+      successModal.style.display = 'none';
+      body.style.overflow = 'auto';
+    },
+    { once: true }
+  );
+}
+
 function handleClick(event: Event) {
   const target = event.target as HTMLElement;
 
@@ -30,8 +53,16 @@ function handleClick(event: Event) {
     openModal();
   }
 
+  if (target.classList.contains('open-success-modal-btn')) {
+    openSuccessModal();
+  }
+
   if (target.classList.contains('close-modal-btn') || target === modal) {
     closeModal();
+  }
+
+  if (target.classList.contains('close-success-modal-btn') || target === successModal) {
+    closeSuccessModal();
   }
 }
 
